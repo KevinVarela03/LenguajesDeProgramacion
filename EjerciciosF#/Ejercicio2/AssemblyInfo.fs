977161c914ff1f3b46fb3fc7d8d4f520e89436c0
Kevin Vinicio Varela Rojas <101670159+KevinVarela03@users.noreply.github.com>
2023-09-14 23:48:16 -0600

    Add files via upload

---
+++ EjerciciosF#/Ejercicio2/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace Ejercicio2.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Ejercicio2")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Ejercicio2")>]
+[<assembly: AssemblyCopyright("Copyright ©  $year$")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("D66A324B-8029-4687-80C0-D5C9F53C2134")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ EjerciciosF#/Ejercicio2/Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+
+let filtrarCadenasConSubcadena subcadena listaCadenas =
+    let contieneSubcadena cadena =
+        (string cadena).Contains(subcadena)
+
+    List.filter contieneSubcadena listaCadenas
+
+[<EntryPoint>]
+let main argv =
+    let subcadena = "la"
+    let listaCadenas = ["la casa"; "el perro"; "pintando la cerca"; "Kevin tiene una nueva camisa, la condenada es linda"]
+
+    let resultado = filtrarCadenasConSubcadena subcadena listaCadenas
+
+    printfn "Cadenas que contienen la subcadena \"%s\":" subcadena
+    List.iter (fun cadena -> printfn "- %s" cadena) resultado
+
+    0
